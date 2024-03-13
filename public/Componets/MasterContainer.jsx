2144e57769b8c7e5d424c6e69a9bf674bf98f60b
Shishir Nepali <155741737+software-wala@users.noreply.github.com>
2024-03-13 12:22:23 +0545

    comment removed

--- public/Componets/MasterContainer.jsx
+++ public/Componets/MasterContainer.jsx
@@ -52,50 +52,6 @@ const MasterContainer = () => {
     },
   ];
 
-  // const bestDealsPhData = [
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/b/m/g/-original-imagxaqtqng2hpxn.jpeg?q=70",
-  //     bestDealsPhName: "redmi 12 5G",
-  //   },
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/h/d/9/-original-imagtc2qzgnnuhxh.jpeg?q=70",
-  //     bestDealsPhName: "Iphone 15 pro max",
-  //   },
-
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/400/400/xif0q/mobile/w/i/d/-original-imagwzrguaee4pz6.jpeg?q=70",
-  //     bestDealsPhName: "Poco x6 pro",
-  //   },
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/400/400/xif0q/mobile/5/y/8/-original-imagtt4mhqrzjs9r.jpeg?q=70",
-  //     bestDealsPhName: "vivo t2 pro",
-  //   },
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/400/400/xif0q/mobile/9/x/f/-original-imagxtnehfg5pcwt.jpeg?q=70",
-  //     bestDealsPhName: "infinix hot 40i",
-  //   },
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/400/400/xif0q/mobile/n/m/f/g34-5g-pb1v0002in-motorola-original-imagwu4rayqhgfjh.jpeg?q=70",
-  //     bestDealsPhName: "moto g34",
-  //   },
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/b/m/g/-original-imagxaqtqng2hpxn.jpeg?q=70",
-  //     bestDealsPhName: "redmi 12 5G",
-  //   },
-  //   {
-  //     bestDealsPhPhoto:
-  //       "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/b/m/g/-original-imagxaqtqng2hpxn.jpeg?q=70",
-  //     bestDealsPhName: "redmi 12 5G",
-  //   },
-  // ];
-
   const sqCardsData1 = [
     {
       sqCardPhoto:
